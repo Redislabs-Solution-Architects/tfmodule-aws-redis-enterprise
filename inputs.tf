6ade5f0e5d71a2dc7e99fcf14d1695b29e3f9bfb
Chris Mague <github@mague.com>
2021-07-01 15:19:42 -0700

    ensure we do not get the eks AMIs

--- inputs.tf
+++ inputs.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "re-ami" {
   most_recent = true
-  name_regex  = "ubuntu-bionic-18.04-amd64-server"
+  name_regex  = "ubuntu\\/images\\/hvm-ssd\\/ubuntu-bionic-18.04-amd64-server"
   # This is Canonical's ID
   owners = ["099720109477"]
 
