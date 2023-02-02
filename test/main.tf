1072a52454ac12262643e86d248aece9dfab88a4
Jason Haugland <jasonhaugland@gmail.com>
2023-02-02 14:46:28 -0500

    fix java and maven version

--- test/main.tf
+++ test/main.tf
@@ -10,7 +10,7 @@ module "mymodule" {
   region           = "us-west-2"
   profile          = "default"
   open-nets        = ["174.141.204.19/32"]
-  data-node-count  = 3
+  data-node-count  = 4
   vpc-cidr         = "10.1.0.0/16"
   vpc-subnets      = ["subnet-047a071c39d0da64d"]
   vpc-id           = "vpc-016b00b2cedc31855"
@@ -22,7 +22,7 @@ module "mymodule" {
   vpc-azs          = ["us-west-2a"]
   cluster-prefix   = "jphterra"
   zone-name        = "demo-rlec.redislabs.com"
-  re-download-url = "https://s3.amazonaws.com/redis-enterprise-software-downloads/6.2.18/redislabs-6.2.18-43-bionic-amd64.tar"
+  re-download-url = "https://s3.amazonaws.com/redis-enterprise-software-downloads/6.2.18/redislabs-6.2.18-65-bionic-amd64.tar"
   common-tags = {
     "Owner"   = "jphaugla"
     "Project" = "jph-prod"
